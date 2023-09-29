f33865b5c33b30a4ed1bc201ec16d99343928be0
esther t <esthertangsq@gmail.com>
2023-09-29 11:32:32 +0800

    29 sept

--- training_yolov8_dd.py
+++ training_yolov8_dd.py
@@ -1,12 +1,15 @@
 from ultralytics import YOLO
 import torch
 
-torch.cuda.set_device(0)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
 # Load a model
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("/home/directwork/dd/runs/detect/train32/weights/best.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="/home/directwork/anaconda3/datasets/combined_new_dataset/data.yaml", epochs=300, patience=30)  # train the model
+model.train(data="/home/directwork/anaconda3/datasets/big_drone_dataset/data.yaml", epochs=300, patience=30)  # train the model
 Metrics = model.val()  # evaluate model performance on the validation set
 results = model("/home/directwork/dd/drones_sky.jpg")  # predict on an image
 path = model.export(format="onnx")  # export the model to ONNX format
